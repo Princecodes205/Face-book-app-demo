433f8d32b1b530b9d54a3012743d18a48d2efe4b
Prince <you@example.com>
2023-10-11 16:20:34 +0100

    New Changes on stories

--- assets/js/home.js
+++ assets/js/home.js
@@ -4,8 +4,13 @@ const seeLess = document.querySelector(".seeLess");
 const stories = document.querySelectorAll(".other-story");
 const fullStoriesSec = document.querySelector(".fullStoriesSec");
 const imageElement = fullStoriesSec.querySelector(".image");
-const leftScrollButton = fullStoriesSec.querySelector(".left-scroll");
-const rightScrollButton = fullStoriesSec.querySelector(".right-scroll");
+const leftScrollButton = document.getElementById("left-scroll-stories");
+const rightScrollButton = document.getElementById("right-scroll-stories");
+const scrollContainer = document.querySelectorAll(".scroll-container");
+const leftScrollButtonStories = document.getElementById("left-scrollOpen");
+const rightScrollButtonStories = document.getElementById("right-scrollOpen");
+const rightScrollButtonReels = document.getElementById("right-scroll-reels");
+const leftScrollButtonReels = document.getElementById("left-scroll-reels");
 
 seeLess.addEventListener("click", (e) => {
   const seeLessDivs = document.querySelectorAll(".seeLessDivs");
@@ -65,43 +70,79 @@ cancelButton.addEventListener("click", () => {
   fullStoriesSec.classList.add("hidden");
 });
 
-const scrollContainer = document.querySelector(".scroll-container");
-const leftScrollButtonStories = document.getElementById("left-scroll");
-const rightScrollButtonStories = document.getElementById("right-scroll");
 const stepSize = 100;
 
 // Add event listener to the left scroll button
-leftScrollButtonStories.addEventListener("click", () => {
+const scrollLeft = () => {
   // Scroll the container to the left
-  scrollContainer.scrollBy({
-    left: -stepSize, // Scroll one container width to the left
-    behavior: "smooth", // Smooth scrolling animation
+  scrollContainer.forEach((container) => {
+    container.scrollBy({
+      left: -stepSize, // Scroll one container width to the left
+      behavior: "smooth", // Smooth scrolling animation
+    });
+    console.log("Clicked");
   });
-  console.log("Clicked");
-});
+};
 
 // Add event listener to the right scroll button
-rightScrollButtonStories.addEventListener("click", () => {
+const scrolRight = () => {
   // Scroll the container to the right
-  scrollContainer.scrollBy({
-    left: stepSize, // Scroll one container width to the right
-    behavior: "smooth", // Smooth scrolling animation
+  scrollContainer.forEach((container) => {
+    container.scrollBy({
+      left: stepSize, // Scroll one container width to the right
+      behavior: "smooth", // Smooth scrolling animation
+    });
+    console.log("I don show");
   });
-});
+};
+
+// function updateScrollButtonsVisibility() {
+//   scrollContainer.forEach((container) => {
+//     // const leftScrollButtonStories = container.querySelector(".left-scroll");
+//     // const rightScrollButtonStories = container.querySelector(".right-scroll");
+
+//     leftScrollButtonStories.style.opacity = container.scrollLeft > 0 ? 0 : 1;
+
+//     // The following condition checks if there is room to scroll to the right
+//     // but also ensures the right button is always visible, as we can scroll in the left direction
+//     rightScrollButtonStories.style.opacity = 1;
+//   });
+// }
 
 // Check if there's room to scroll and hide/show buttons accordingly
-function updateScrollButtonsVisibility() {
-  leftScrollButtonStories.style.opacity =
-    scrollContainer.scrollLeft > 0 ? 1 : 0;
-  rightScrollButtonStories.style.display =
-    scrollContainer.scrollLeft <
-    scrollContainer.scrollWidth - scrollContainer.clientWidth
-      ? 1
-      : 0;
-}
+// function updateScrollButtonsVisibility() {
+//   scrollContainer.forEach((container) => {
+//     // const leftScrollButton = container.querySelector(".left-scroll");
+//     container.offsetWidth;
+//     leftScrollButtonStories.style.opacity = container.scrollLeft > 0 ? 1 : 0;
+
+//     rightScrollButtonStories.style.opacity =
+//       container.scrollLeft < container.scrollWidth - container.clientWidth
+//         ? 1
+//         : 0;
+//   });
+//   // leftScrollButtonStories.style.opacity = scrollContainer.forEach(
+//   //   (container) => {
+//   //     container.scrollLeft > 0 ? 1 : 0;
+//   //   },
+//   //   console.error("an error")
+//   // );
+//   // rightScrollButtonStories.style.display =
+//   //   scrollContainer.scrollLeft <
+//   //   scrollContainer.scrollWidth - scrollContainer.clientWidth
+//   //     ? 1
+//   //     : 0;
+// }
 
 // Add a scroll event listener to the container
-scrollContainer.addEventListener("scroll", updateScrollButtonsVisibility);
+// scrollContainer.forEach((container) => {
+//   container.addEventListener("scroll", updateScrollButtonsVisibility());
+// });
+leftScrollButtonStories.addEventListener("click", scrollLeft);
+leftScrollButtonReels.addEventListener("click", scrollLeft);
+
+rightScrollButtonStories.addEventListener("click", scrolRight);
+rightScrollButtonReels.addEventListener("click", scrolRight);
 
 // Initially, check the visibility of the scroll buttons
 updateScrollButtonsVisibility();
